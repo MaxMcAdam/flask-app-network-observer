bd7fa3689dadc5ccfe68b608fc738ddcd5563d2b
Maxwell-McAdam <Maxwell.McAdam@ibm.com>
2019-04-22 14:49:46 -0400

    added method to remove an authorized host from list

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import couchdb
 
 app = Flask(__name__)
 
-@app.route('/api/v0.0.1/auth-hosts', methods=['POST'])
+@app.route('/api/auth-hosts/add/', methods=['POST'])
 def new_auth_host():
     if not request.json:
         abort(400)
-    #post_content=request.get_json()
+
     auth_host = {
             'hostname': {
         	'name': request.json.get('name'),
@@ -18,15 +18,20 @@ def new_auth_host():
             'persistent': request.json.get('persistent'),
             'devdesc': request.json.get('devdesc')
     }
-    print("Auth-host extracted from post data")
-    post_auth_hosts_db(auth_host)
-    return jsonify({'auth-host': auth_host}), 201
 
-def post_auth_hosts_db(auth_host):
     couchserver = couchdb.Server("http://127.0.0.1:5984/")
     db = couchserver['auth-hosts']
     doc_id, doc_rev = db.save(auth_host)
+    return jsonify({'auth-host': auth_host}), 201
 
+@app.route('/api/auth-hosts/remove/<int:id>')
+def remove_auth_host():
+    if not request.json:
+        abort(400)
+    couchserver = couchdb.Server("http://127.0.0.1:5984/")
+    db = couchserver['auth-hosts']
+    output = db.delete(id)
+    return output
 
 if __name__ == '__main__':
     app.run(debug=True)
